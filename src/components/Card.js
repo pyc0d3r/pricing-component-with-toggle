047d55abea55e4b2c52896ebf0e02d4ceea0d494
pyc0d3r <102415486+pyc0d3r@users.noreply.github.com>
2022-07-04 19:25:52 +0300

    Change width of cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,10 @@ function Card({ name, price, index, children, isMonthly }) {
   return (
     <div className={`card card-${index}`} >
       <h2>{name}</h2>
-      <h3 ref={priceRef} className="card-price-monthly">${price}</h3>
+      <h3 ref={priceRef} className="card-price-monthly">
+        <span className="dollar-sign">$</span>
+        <span className="price">{price}</span>
+      </h3>
       {children}
       <button>Learn More</button>
     </div >
